555d8eee253438000601dd82e8ff7521430f50e5
DevLeonardoK <devleonardok@gmail.com>
2026-07-13 08:04:25 -0300

    feat(spark): consume market_data_raw_topic via structured streaming
    Le o topico dos 3 brokers com readStream, projeta key/value como string
    e escreve no sink console. Substitui o esqueleto que so abria/fechava a
    SparkSession.

--- python/spark_instance.py
+++ python/spark_instance.py
@@ -1,10 +1,33 @@
 from pyspark.sql import SparkSession
+import pyspark.sql.functions as F
+from pyspark.sql.types import StringType
 
-spark = (
-    SparkSession.builder
-    .appName("market_data_spark")
-    .master("spark://apache-spark-master:7077")
-    .getOrCreate()
-)
+try:
+    spark = (
+        SparkSession.builder
+        .appName("market_data_spark")
+        .master("spark://apache-spark-master:7077")
+        .getOrCreate()
+    )
+    
+    df = (
+        spark.readStream
+        .format("kafka")
+        .option("kafka.bootstrap.servers", "kafka_broker_1:5091,kafka_broker_2:5092,kafka_broker_3:5093")
+        .option("subscribe", "market_data_raw_topic")
+        .option("startingOffsets", "latest")
+        .load()
+    )
 
-spark.stop()
+    query = (
+        df.select(
+            F.col("key").cast(StringType()),
+            F.col("value").cast(StringType())
+        ).writeStream.format("console").option("truncate", "false")
+        .start()
+    )
+
+    query.awaitTermination()
+
+except Exception:
+    raise
